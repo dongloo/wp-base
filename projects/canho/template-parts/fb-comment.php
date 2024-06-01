3ba8f0dcfdc9358033f242d2c9a4055f461b5b6c
Dong Loo <minhkidong@gmail.com>
2024-06-01 19:31:21 +0700

    update

--- projects/canho/template-parts/fb-comment.php
+++
@@ -1,4 +0,0 @@
-<div id="owl-fb-comments">
-    <div class="fb-comments" xid="<?php the_ID(); ?>" data-width="100%" data-numposts="20" data-colorscheme="light" data-version="v2.3"></div>
-</div>
-<?php //comments_template (); ?>
